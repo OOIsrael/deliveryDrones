b5536895484d30ea645d4974402347e27b7be571
ooisrael <dareoni27@gmail.com>
2022-09-15 19:01:26 +0100

    New Feat: Addition of medication api

---
+++ src/main/java/com/oluwadamilareisrael/deliveryDrones/medication/Medication.java
@@ -0,0 +1,4 @@
+package com.oluwadamilareisrael.deliveryDrones.medication;
+
+public class Medication {
+}
---
+++ src/main/java/com/oluwadamilareisrael/deliveryDrones/medication/MedicationRepository.java
@@ -0,0 +1,4 @@
+package com.oluwadamilareisrael.deliveryDrones.medication;
+
+public interface MedicationRepository {
+}
